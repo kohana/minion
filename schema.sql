d8f033f22a30459f9f3c047d83bc3b947c587c4a
Matt Button <matthew@sigswitch.com>
2010-12-30 04:27:20 +0000

    Adding schema dump for migrations table

---
+++ schema.sql
@@ -0,0 +1,11 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `timestamp` varchar(12) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `applied` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`timestamp`,`location`),
+  UNIQUE KEY `MIGRATION_ID` (`timestamp`,`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
