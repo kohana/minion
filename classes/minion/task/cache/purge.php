3a0067b78e03a1ee6fdfc5ef3c07644da731006e
Matt Button <matthew@sigswitch.com>
2011-01-24 17:44:17 +0000

    Updated the cache:purge task to actually purge caches

--- classes/minion/task/cache/purge.php
+++ classes/minion/task/cache/purge.php
@@ -1,28 +1,48 @@
 <?php
 
 /**
- * Purges the application file cache
+ * Purges the application, requires at least one cache configuration group to be 
+ * specified
+ *
+ *
+ * Available config options: 
+ *
+ * --cache=cache1[,cache2,cache2...]
+ *  
+ *  Specify the caches to clear, each item in the list is the key of a cache 
+ *  config group in config/cache.php
+ *
+ *  This is a required config option
  *
  * @author Matt Button <matthew@sigswitch.com>
- **/
+ */
 class Minion_Task_Cache_Purge extends Minion_Task
 {
-
 	/**
-	 * Gets a set of config options this minion task accepts
-	 *
-	 * @return array
+	 * An array of config options that this task can accept
 	 */
-	public function get_config_options()
-	{
-		return array();
-	}
+	protected $_config = array();
 
 	/**
 	 * Clears the cache
 	 */
 	public function execute(array $config)
 	{
-	
+		if(empty($config['cache']))
+		{
+			return 'Please specify a set of cache configs.';
+		}
+
+		$config['cache'] = trim($config['cache'], ',');
+
+		$caches = explode(',', $config['cache']);
+
+		foreach($caches as $cache)
+		{
+			Cache::instance($cache)
+				->delete_all();
+		}
+
+		return 'Cleared caches for '.$config['cache'];
 	}
 }
