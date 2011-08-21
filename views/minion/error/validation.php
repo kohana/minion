1fba652891c8c6373182225c6eec877765070584
Lorenzo Pisani <zeelot3k@gmail.com>
2011-08-21 12:44:14 -0700

    adding a view for displaying validation errors (refs #43)

---
+++ views/minion/error/validation.php
@@ -0,0 +1,10 @@
+Parameter Errors:
+<?php foreach ($errors as $parameter => $error): ?>
+    <?php echo $parameter; ?> - <?php echo $error; ?>
+<?php endforeach; ?>
+
+Run
+
+    index.php --uri=minion
+
+for more help
