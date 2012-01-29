736707b8440fed88d9c4fe6e25166589eb27ede2
Lorenzo Pisani <zeelot3k@gmail.com>
2012-01-29 15:28:29 -0800

    adding missing SYSPATH checks

--- classes/cli.php
+++ classes/cli.php
@@ -1,3 +1,3 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
 class CLI extends Minion_CLI {}
--- classes/minion/cli.php
+++ classes/minion/cli.php
@@ -1,4 +1,4 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
 class Minion_CLI {
 
