1ff202b3d16054f5620e0aa74d3240ac2b5f9857
Cyril <cgraze@gmail.com>
2014-10-31 18:03:43 +0000

    Unit test for Minion_Task::factory()

--- tests/minion/TaskTest.php
+++ tests/minion/TaskTest.php
@@ -68,4 +68,31 @@ class Minion_TaskTest extends Kohana_Unittest_TestCase {
 		$this->assertSame($expected, Minion_Task::convert_class_to_task($class));
 	}
 
+	/**
+	 * Provides test data for test_factory().
+	 *
+	 * @return array
+	 */
+    public function provider_factory()
+    {
+        return array(
+            array(array('task' => 'help')),
+            array(array('help')),
+        );
+    }
+
+	/**
+	 * Tests that the factory can instantiate with or without the --task CLI param
+	 *
+	 * @test
+	 * @covers Minion_Task::factory
+	 * @dataProvider provider_factory
+	 * @param array Options as would be returned from Minion_CLI::options
+	 */
+
+    public function test_factory($options)
+    {
+        $this->assertInstanceOf('Minion_Task', Minion_Task::factory($options));
+    }
+
 }
