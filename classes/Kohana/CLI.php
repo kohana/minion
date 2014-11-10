abd34d68af4ea0c3aa94dc3ce8b0d139e6a314d4
cyrgit <cyrgit@users.noreply.github.com>
2014-11-10 16:08:39 +0100

    Deprecates Fuel Team color(), use CLImate output for colors

--- classes/Kohana/CLI.php
+++ classes/Kohana/CLI.php
@@ -1,17 +1,29 @@
 <?php
 /**
-* Minion helper class, interact with the command line by accepting input options.
-*
-* @package   Kohana/Minion
-* @category  Helper
-* @author    Kohana Team
-* @copyright (c) 2009-2014 Kohana Team
-* @license   http://kohanaframework.org/license
-*/
+ * CLI factory. Used to build I/O streams
+ *
+ * // STDOUT
+ * $output  = CLI::factory('Output');
+ *
+ * // Souped-up colors and formatting
+ * $output  = CLI::factory('CLImate');
+ *
+ * // STDIN
+ * $options = CLI::factory('Input');
+ *
+ * // Command line parameters
+ * $options = CLI::factory('Options');
+ *
+ * @package   Kohana/Minion
+ * @category  Helper
+ * @author    Kohana Team
+ * @copyright (c) 2009-2014 Kohana Team
+ * @license   http://kohanaframework.org/license
+ */
 abstract class Kohana_CLI {
 
 	/**
-	 * 
+	 *
 	 * @param string $name
 	 * @param array  $options
 	 * @return \Kohana_Minion_CLI
@@ -26,26 +38,4 @@ abstract class Kohana_CLI {
 		}
 		throw new Minion_Exception("CLI interface $class not found.");
 	}
-
-	/**
-	 * Returns the given text with the correct color codes for a foreground 
-	 * and optionally a background color.
-	 *
-	 * @author    Fuel Development Team
-	 * @license   MIT License
-	 * @copyright 2010 - 2011 Fuel Development Team
-	 * @link      http://fuelphp.com
-	 * 
-	 * @param  string $text the text to color
-	 * @param  atring $foreground the foreground color, uses [$this->$_foreground_colors]
-	 * @param  string $background the background color, uses [$this->$_background_colors]
-	 * @return string the color coded string
-	 * @throws Minion_Exception
-	 * @uses   Kohana::$is_windows
-	 */
-	public function color($text, $foreground, $background = NULL)
-	{
-		return $text;
-	}
-
 }
